074a7b5f5019ce68e3581e77016bfd243f979e9b
bolliger32 <bolliger32@gmail.com>
2013-06-25 12:00:19 -0500

    resets to full functions

--- sw_engineering/animals.py
+++ sw_engineering/animals.py
@@ -1,44 +1,15 @@
 def mean_animals(filename, kind):
-    """
-    Calculates the mean number of a particular animal seen per sighting using
-    the data in the given file.
-
-    Parameters
-    ----------
-    filename : str
-        Path to file to use.
-    kind : str
-        Species of animal for which to calculate average
-        number seen per sighting.
-
-    Returns
-    -------
-    mean : float
-        Mean number of animal seen per sighting.
-
-    """
     date, time, species, count = read_animals(filename)
-    date, time, species, count = \
-        filter_animals_by_kind(kind, date, time, species, count)
+    date, time, species, count = filter_animals(kind, date, time, species, count)
     return mean(count)
-
 
 def read_animals(filename):
     """
-    Reads an animals.txt type file. Returns four lists, one per column.
+    Reads an animals type file. Returns four lists, one per column.
 
-    Parameters
-    ----------
-    filename : str
-        The name of an animals.txt type file.
+    Input is a filename.
 
-    Returns
-    -------
-    dates : list of str
-    times : list of str
-    species : list of str
-    counts : list of int
-
+    Order of returned items: date, time, species, count.
     """
     f = open(filename)
     dates = []
@@ -57,48 +28,27 @@ def read_animals(filename):
 
     return dates, times, species, counts
 
+
 def mean(nums):
-    """
-    Calculates the mean of a list of numbers.
+    total = 0
 
-    Parameters
-    ----------
-    nums : list of numbers
+    for n in nums:
+        total = total + n
 
-    Returns
-    -------
-    m : number
-
-    """
-    avg=None
-    if len(nums):
-        avg=float(sum(nums))/len(nums)
-    else:
-        print 'error empty list'
-    return avg
-        
+    return float(total) / len(nums)
 
 
-def filter_animals_by_kind(kind, date, time, species, count):
-    """
-    Filter the animals dataset to only include data for a given species
-    of animal.
+def filter_animals(kind, date, time, species, count):
+    fdate = []
+    ftime = []
+    fspecies = []
+    fcount = []
 
-    Parameters
-    ----------
-    kind : str
-        Species of animal to filter on.
-    date : list of str
-    time : list of str
-    species : list of str
-    count : list of int
+    for i in range(len(date)):
+        if species[i] == kind:
+            fdate.append(date[i])
+            ftime.append(time[i])
+            fspecies.append(species[i])
+            fcount.append(count[i])
 
-    Returns
-    -------
-    filtered_date : list of str
-    filtered_time : list of str
-    filtered_species : list of str
-    filtered_count : list of int
-
-    """
-    # fill this in
+    return fdate, ftime, fspecies, fcount
--- sw_engineering/test_animals.py
+++ sw_engineering/test_animals.py
@@ -2,24 +2,52 @@ import animals
 
 
 def test_read_animals():
-    ref_dates = ['2011-04-22', '2011-04-23', '2011-04-23', '2011-04-23', '2011-04-23']
-    ref_times = ['21:06', '14:12', '10:24', '20:08', '18:46']
-    ref_species = ['Grizzly', 'Elk', 'Elk', 'Wolverine', 'Muskox']
-    ref_counts = [36, 25, 26, 31, 20]
+    date, time, species, count = animals.read_animals('animals.txt')
 
-    dates,times,species,counts = animals.read_animals('animals.txt')
-    
-    assert dates == ref_dates
-    assert times == ref_times
-    assert species == ref_species
-    assert counts == ref_counts
+    true_date = ['2011-04-22', '2011-04-23', '2011-04-23', '2011-04-23', '2011-04-23']
+    true_time = ['21:06', '14:12', '10:24', '20:08', '18:46']
+    true_species = ['Grizzly', 'Elk', 'Elk', 'Wolverine', 'Muskox']
+    true_count = [36, 25, 26, 31, 20]
+
+    assert date == true_date, 'Dates not the same!'
+    assert time == true_time, 'Times not the same!'
+    assert species == true_species, 'Species not the same!'
+    assert count == true_count, 'Counts not the same!'
+
 
 def test_mean():
-    assert animals.mean([0,1]) == .5
-    assert animals.mean([-4,6]) == 1
-    assert animals.mean([0, 1.5, 4.5]) == 2
-    assert animals.mean([5]) == 5
-    assert animals.mean([-5,-15,-10]) == -10
-    assert animals.mean([1e6,1e7]) == 5.5e6
-    assert animals.mean([-4,2,4,-2]) == 0
-    
+    mean = animals.mean([4, 5, 6])
+    assert mean == 5
+
+    mean = animals.mean([10])
+    assert mean == 10
+
+    mean = animals.mean([10, 7])
+    assert mean == 8.5
+
+
+def test_filter_animals():
+    date, time, species, count = animals.read_animals('animals.txt')
+    kind = 'Grizzly'
+    d, t, s, c = animals.filter_animals(kind, date, time, species, count)
+
+    assert d == ['2011-04-22']
+    assert t == ['21:06']
+    assert s == ['Grizzly']
+    assert c == [36]
+
+    kind = 'Elk'
+    d, t, s, c = animals.filter_animals(kind, date, time, species, count)
+
+    assert d == ['2011-04-23', '2011-04-23']
+    assert t == ['14:12', '10:24']
+    assert s == ['Elk', 'Elk']
+    assert c == [25, 26]
+
+
+def test_mean_animals():
+    mean = animals.mean_animals('animals.txt', 'Grizzly')
+    assert mean == 36
+
+    mean = animals.mean_animals('animals.txt', 'Elk')
+    assert mean == 25.5
